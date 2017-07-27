4cb613e6e9a1740c80b91a4320946d5c893789e5
John Komarkowski <John.Komarkowski@datagate-i.com>
2017-07-27 23:22:32 +1200

    27/07/2017

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Vidly.Controllers;
 
 namespace Vidly
 {
@@ -13,6 +14,9 @@ namespace Vidly
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+
+      
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModel;
+
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+
+            //var  viewResult = new ViewResult();
+            //viewResult.ViewData.Model
+
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Customer 1"},
+                new Customer {Name = "Customer 2"}
+            };
+
+            var ViewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+
+            return View(ViewModel);
+
+
+        }
+      
+         [Route("movies/released/{year:regex(\\d{4}):range(1987,2099)}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int? year, int? month)
+        {
+            if (!year.HasValue)
+                year = 2016;
+            if (!month.HasValue)
+                month = 01;
+
+            return Content(year + "/" + month); 
+        }
+            
+    }
+}
